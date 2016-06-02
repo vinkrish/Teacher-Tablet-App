d16be1a78acce223fcf2161b42aca3111f06b45e
Vinay Krishna <vinaykrishna1989@gmail.com>
2016-06-02 21:08:12 +0530

    server api constants

---
+++ app/src/main/java/in/teacher/sync/StringConstant.java
@@ -0,0 +1,15 @@
+package in.teacher.sync;
+
+/**
+ * Created by vinkrish on 31/08/15.
+ */
+public interface StringConstant {
+    String logged = "http://127.0.0.1/schoolonweb/app_admin/views/sync/log/index.php";
+    String request_first_time_sync = "http://127.0.0.1/schoolonweb/app_admin/views/sync/new_sync/request_first_time_sync_new_feature.php";
+    String ask_for_download_file = "http://127.0.0.1/schoolonweb/app_admin/views/sync/new_sync/ask_for_download_file_newapk.php";
+    String block_a_tab = "http://127.0.0.1/schoolonweb/app_admin/views/sync/new_sync/block_a_tab_newapk.php";
+    String update_downloaded_file = "http://127.0.0.1/schoolonweb/app_admin/views/sync/new_sync/update_downloaded_file.php";
+    String update_processed_file = "http://127.0.0.1/schoolonweb/app_admin/views/sync/new_sync/update_processed_file.php";
+    String acknowledge_uploaded_file = "http://127.0.0.1/schoolonweb/app_admin/views/sync/new_sync/acknowledge_uploaded_file.php";
+    String TAG_SUCCESS = "success";
+}
---
+++ app/src/main/java/in/teacher/util/Constants.java
@@ -0,0 +1,13 @@
+package in.teacher.util;
+
+/**
+ * Created by vinkrish on 31/08/15.
+ */
+public class Constants {
+    public static final String AWS_ACCOUNT_ID = "277213702081";
+    public static final String COGNITO_POOL_ID =
+            "us-east-1:fec1c900-baa2-4aa7-b83f-9df4a2eec659";
+    public static final String COGNITO_ROLE_UNAUTH =
+            "arn:aws:iam::277213702081:role/Cognito_SchoolComAppUnauth_Role";
+    public static final String BUCKET_NAME = "demo";
+}
